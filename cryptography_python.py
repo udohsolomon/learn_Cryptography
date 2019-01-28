9eb46d21528436b6af012dd186012f8f56fd876e
Solomon <solomonlazio@gmail.com>
2019-01-28 12:32:06 +0000

    first commit

---
+++ cryptography_python.py
@@ -0,0 +1,11 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+import base64
+
+for _hash in [hashes.SHA1, hashes.SHA224, hashes.SHA256, hashes.SHA384, hashes.SHA512]:
+    digest = hashes.Hash(_hash(), backend=default_backend())
+    digest.update(b"Cryptography 2019")
+    # digest.update(b"2019")
+    msg_digest = digest.finalize()
+    # Notice the output size of the digest
+    print(_hash.name, len(msg_digest), len(msg_digest) * 8, base64.b64encode(msg_digest))
