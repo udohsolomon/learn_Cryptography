5be5d75a74e2e831ff5345b58c856a0cb0a921c0
Solomon <solomonlazio@gmail.com>
2019-01-30 03:02:04 +0000

    update notebook

--- cryptography_python.py
+++ cryptography_python.py
@@ -1,6 +1,6 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-import base64
+import base64 # to produce human readable encoding of the binary bytes
 
 for _hash in [hashes.SHA1, hashes.SHA224, hashes.SHA256, hashes.SHA384, hashes.SHA512]:
     digest = hashes.Hash(_hash(), backend=default_backend())
